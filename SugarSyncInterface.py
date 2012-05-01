64c027a7fbff78dbf0ceb7767bfd5f212cd78e36
Benjamin Welton <welton@wotan.cs.wisc.edu>
2012-05-01 11:27:46 -0500

    Added SugarSync Interface

---
+++ SugarSyncInterface.py
@@ -0,0 +1,371 @@
+from pysugarsync import SugarSync
+import ConfigParser
+import datetime
+
+class SugarSyncWrapper(object):
+    def __init__(self, filename=None):
+        """ Setup's parameters for connection to service. Does other initialization
+
+            Takes in a filename (string) returns nothing
+        """
+        self._cache = {}   # Directory Referece Cacheing
+        conf = ConfigParser.RawConfigParser()
+        conf.read(filename)
+        self._sync = SugarSync(conf=conf)
+        self._tldChanged = True
+
+    def GetTLD (self):
+        """ Gets the top level directory/file information. 
+
+            returns a hash table: 
+                ret = { }
+            where every directory/file has a key with the following
+            output:
+                ret["somedirname"] = self
+                self =  this class
+        """
+        ret = {}
+        tmp = self._sync.ListSyncFolders()
+        for x in tmp:
+            ret[x.GetName()] = self
+            self._cache[x.GetName()] = x
+        self._tldChanged = False
+        return ret
+
+    def _RecursiveLookup (self, prev, name, fullpath, path):
+        local = None
+        loc = fullpath.index(name)
+        pathname = "/".join(fullpath[:loc]) + "/" + name
+
+        if pathname not in self._cache:
+            subpath = "/".join(fullpath[:loc])
+            dirs = self._sync.GetFolders(prev)
+            for x in dirs:
+                self._cache[subpath + "/" + x.GetName()] = x
+
+            files = self._sync.ListFiles(prev)
+            for x in files:
+                self._cache[subpath + "/" + x.GetName()] = x
+
+            if pathname not in self._cache:
+                return None
+
+        local = self._cache[pathname]
+        
+        if len(path) == 0:
+            return local
+        if len(path) >= 2:
+            return self._RecursiveLookup(local, path[0], fullpath, path[1:])        
+        else:
+            return self._RecursiveLookup(local, path[0], fullpath, [])
+
+    def _LookupPathname (self, pathname):
+        """ Performs pathname lookup to find SyncFile class
+            
+            Returns a syncfile class for the path specified (None if does not exist)
+        """
+        path =  pathname.split("/")
+
+        if self._tldChanged:
+            self.GetTLD()
+
+        prev = None
+
+        if path[0] in self._cache:
+            prev = self._cache[path[0]]
+        
+        if prev == None:
+            return
+        
+        if len(path) == 1:
+            return prev
+
+        return self._RecursiveLookup(prev, path[1], path, path[2:])
+
+    def Read (self, pathname, offset, size): 
+        """ similar to fuse. returns data as a string 
+        
+            returns a dictionary with the following keys
+
+                ret["status"] = True for completed, False for failed
+                ret["data"] = data read from the file
+        """
+        ret = {}
+        fileID = self._LookupPathname(pathname)
+        data = self._sync.GetFile(fileID)
+        ret["status"] = True
+        if offset + size < len(data[1]) and size != -1:
+            ret["data"] = data[1][offset:offset+size]
+        else:
+            ret["data"] = data[1][offset:]
+        return ret
+
+    def Write(self, pathname, data):
+        """ Write data out to the file specified.
+
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+        """
+        ret = {}
+        fileID = self._LookupPathname(pathname)
+        ret["status"] = self._sync.WriteFile(fileID, data)
+        return ret
+       
+    def Mknode (self, pathname): 
+        """ Make a new file in the pathname specified. 
+
+            pathname = string
+            
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+        """
+        ret = {}
+        folderpath = "/".join(pathname.split("/")[:-1])
+        folderID = self._LookupPathname(folderpath)
+        ret["status"] = self._sync.CreateFile(folderID, pathname.split("/")[-1])
+        return ret
+
+    def Unlink (self, pathname):
+        """ Deletes a file from the service
+
+            pathname = string
+
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+        """
+        ret = {}
+        fileID = self._LookupPathname(pathname)
+        ret["status"] = self._sync.DeleteFile(fileID)
+        if ret["status"] == True:
+            del self._cache[pathname]
+        return ret
+
+    def GetPermissionFile (self):
+        """ Gets the data inside of the permission file stored on the service
+        
+            returns a dictionary with the following keys:
+                   
+                ret["status"] = True for completed, False for failed
+                ret["data"] = Permission file data (hash table: hash["pathname"] = permissions).
+                              Permissions are as follows [UID,GID,MODE (777 - octet format)]
+        """
+        ret = {}
+        permissionFile = "Magic Briefcase/pycloudgate/permissions.txt"
+        if self._LookupPathname("Magic Briefcase/pycloudgate") == None:
+            self.Mkdir("Magic Briefcase/pycloudgate")
+            self._tldChanged = True
+        if self._LookupPathname(permissionFile) == None:
+            self.Mknode(permissionFile)
+            self.Write(permissionFile, "novalue,000,000,10000\n")
+
+        data = self.Read(permissionFile, 0, -1)
+        tmpme = {}
+        for x in data["data"].split("\n"):
+            if "novalue,000,000,10000" == x or "" == x:
+                continue
+            tmp =  x.split(",")
+            tmpme[tmp[0]] = [int(tmp[1],10), int(tmp[2],10), int(tmp[3],10)]
+
+        ret["status"] = True
+        ret["data"] = tmpme
+        return ret
+
+    def WritePermissions (self, hashtable):
+        """ Writes the permissions data to the permission file.
+
+            hashtable - hash table of same format as GetPermissionsFile()
+
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+        """
+        ret = {}
+        permissionFile = "Magic Briefcase/pycloudgate/permissions.txt"
+        data = ""
+        for x in hashtable:
+            data += x + "," + str(hashtable[x][0]) + "," + str(hashtable[x][1]) + "," + str(hashtable[x][2]) + "\n"
+        ret = self.Write(permissionFile, data)
+        return ret
+
+    def Truncate(self, filename, pos):
+        """ Truncates a file to the position specified 
+
+            filename - string
+            pos - integer showing what position to truncate the file too
+
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+        """
+        ret = {}
+        data =  self.Read(filename, 0, pos)
+        ret = self.Write(filename, data["data"])
+        return ret
+
+    def Mkdir (self, pathname):
+        """ Makes the directory specified 
+
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+        """
+        ret = {}
+        path =  pathname.split("/")
+        dir = self._LookupPathname("/".join(path[:-1]))
+        ret["status"] = self._sync.CreateFolder(dir, path[-1])
+        return ret
+
+    def Readdir (self, path):
+        """ Return a directory listing for the given directory at offset
+            
+            returns a dictionary with the following keys:
+
+                ret["status"] = True for completed, False for failed
+                ret["list"] = Directory list
+        """
+        ret = {}
+        dir = self._LookupPathname(path)
+
+        pathlist = self._sync.GetFolders(dir)
+        pathlist += self._sync.ListFiles(dir)
+       
+        ret["list"] = pathlist
+
+        return ret
+
+    def GetAttr (self, path):
+        """ Returns the attributes for the file (stat structure)
+
+            returns a dictionary with the following keys:
+                ret["status"] = True for completed, False for Failed
+                ret["st_size"] = size (bytes) of the file
+                ret["st_mtime"] = datetime class with last modification
+
+        """
+        ret = {}
+        if path in self._cache:
+
+            ret["status"] = True
+            if self._cache[path].GetType() == "SyncFile":
+                ret["st_size"] = int(self._cache[path].GetSize())
+                ret["st_mtime"] = self._cache[path].GetModified()
+            else:
+                ret["st_size"] = 0
+                ret["st_mtime"] = datetime.datetime.now()
+        else:
+            file =  self._LookupPathname(path)
+            if file != None:
+                ret["status"] = True
+            else:
+                ret["status"] = False
+                return ret
+
+            if self._cache[path].GetType() == "SyncFile":
+                ret["st_size"] = int(self._cache[path].GetSize())
+                ret["st_mtime"] = self._cache[path].GetModified()
+            else:
+                ret["st_size"] = 0
+                ret["st_mtime"] = datetime.datetime.now()
+        return ret
+
+## Minor Testing            
+if __name__ == "__main__":
+    # init
+    t_class = SugarSyncWrapper("conf.ini")
+
+    # Test GetTLD()
+    tlds = t_class.GetTLD()
+    for x in tlds:
+        print x
+        
+
+    # Test Mknode
+    filedir = "Mobile Photos/testfile.png"
+    status = t_class.Mknode(filedir)
+    if status["status"] == True:
+        print "File created correctly"
+    else:
+        print "File not created correctly"
+
+    # Test Write
+    data = "It's not a pyramid scheme, it's a triangle of opportunity."
+    ret = t_class.Write(filedir, data)
+    if ret["status"] == True:
+        print "File correctly written"
+    else:
+        print "File incorrectly written"
+
+    ret = t_class.Read(filedir, 0, -1)
+    if ret["status"] ==  True:
+        if ret["data"] == data:
+            print "Data Read Correctly"
+        else:
+            print "Data does not match"
+    else:
+        print "Could not read data!"
+    
+    status =  t_class.Unlink(filedir)
+    if status["status"] == True:
+        print "File Successfully Deleted"
+    else:
+        print "File failed to delete"
+
+    permfile = t_class.GetPermissionFile()
+    if permfile["status"] == False:
+        print "Could not get permission file"
+    else:
+        print "Permissions found!"
+
+    fakePermissions = {"testperm": [1000, 2000, int("777",10)]}
+    status = t_class.WritePermissions(fakePermissions)
+    if status["status"] == False:
+        print "Did not write out permission file correctly"
+    else:
+        print "Permission file written correctly"
+
+    permfile = t_class.GetPermissionFile()
+    if permfile["status"] == True:
+        if permfile["data"]["testperm"] == [int("1000",10), int("2000",10), int("777",10)]:
+            print "Permission retrieved Correctly"
+        else:
+            print "Permission did not save correctly"
+    else:
+        print "Could not open permission file"
+
+    ## Test Truncate
+    filedir = "Mobile Photos/testfile.png"
+    status = t_class.Mknode(filedir)
+    data = "HELLO WORLD!!!!!!"
+    status = t_class.Write(filedir, data)
+    status = t_class.Truncate(filedir, 5)
+    if status["status"] ==  True:
+        d = t_class.Read(filedir, 0, -1)
+        if d["data"] == "HELLO":
+            print "Truncate works correctly"
+        else:
+            print "NOT VALID DATA FOR TRUNCATE GOT " + str(d["data"])
+    else:
+        print "Truncate not successful"
+
+    status =  t_class.Unlink(filedir)
+
+    status = t_class.Readdir("Mobile Photos")
+    for x in status["list"]:
+        print x
+
+    status = t_class.GetAttr("Mobile Photos")
+    if status["status"] == True:
+        print status["st_size"]
+        print status["st_mtime"]
+    else:
+        print "GETTING ATTR FAILED"
+    status = t_class.GetAttr("Magic Briefcase/pycloudgate/permissions.txt")
+    if status["status"] == True:
+        print status["st_size"]
+        print status["st_mtime"]
+    else:
+        print "GETTING ATTR FAILED"
+
