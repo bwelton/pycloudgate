5025466fa86f4463d2d0707297ab4cac7d51559c
Benjamin Welton <welton@wotan.cs.wisc.edu>
2012-05-04 14:58:28 -0500

    Added CacheClass functionality

--- src/CacheBase.py
+++ src/CacheBase.py
@@ -4,26 +4,54 @@ class CacheClass(object):
     def __init__(self): 
         """ Initializes Cache Class """
         self._cacheMap = {}
+        self._openCounts = {}
 
     def OpenCache(self, filename, buf):
         """ Opens a cache file the data buffer contained in buf written to it """
+        if filename in self._cacheMap:
+            self._openCounts[filename] += 1        
         self._cacheMap[filename] =  tempfile.TemporaryFile()
-        pass
+        self._cacheMap[filename].write(buf)
+        self._openCounts[filename] = 1
 
     def Write(self, filename, buf, offset):
         """ Write this buffer to the offset specified in a temporary file """
-        pass
+
+        if filename not in self._cacheMap:
+            return False
+
+        f = self._cacheMap[filename]
+        
+        f.seek(offset, 0)
+        f.write(buf)
+        return True
+          
+    
+    def read(self, filename, offset, length):
+        """ Read a cached file """
+        if filename not in self._cacheMap:
+            return None
+
+        f = self._cacheMap[filename]
+        f.seek(offset, 0)
+        data = f.read(length)
+        return data 
+
 
     def Close(self, filename):
         """ read the data from the tempfile, close the tempfile, return the data"""
 
-        pass
-
-
-
-
+        if filename not in self._cacheMap:
+            return None
         
-
+        f =  self._cacheMap[filename]
+        f.seek(0,0)
+        data = f.read()
+        if self._openCounts[filename] == 1:
+            f.close()
+            del self._cacheMap[filename]
+            del self._openCounts[filename]
+        else:
+            self._openCounts[filename] = self._openCounts[filename] - 1
+        return data
         
-
-
